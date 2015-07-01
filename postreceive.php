1803ac8a79b41f8f4339171d17febac97c2022ea
vruechardt <valentin.ruechardt@cdtm.de>
2015-07-01 14:45:07 +0200

    Hook test

--- postreceive.php
+++ postreceive.php
@@ -1,3 +1,3 @@
 <?php
 
-shell_exec( 'cd /kunden/hompages/23/d582310753/htdocs/Charade/ && git reset --hard HEAD && git pull' );
+shell_exec( 'cd /kunden/hompages/23/d582310753/htdocs/Charade/ && git pull' );
